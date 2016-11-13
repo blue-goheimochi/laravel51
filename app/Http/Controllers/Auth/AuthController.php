6b9ebb8b5e7c5cca59200aeb94ded1515f2cae16
blue-goheimochi <ooohhhaaassshhhiii@gmail.com>
2016-11-13 19:52:52 +0900

    Users.phpをDataAccess\Eloquentに移動

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers\Auth;
 
-use App\User;
+use App\DataAccess\Eloquent\User;
 use Validator;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesAndRegistersUsers;
@@ -11,7 +11,7 @@ class AuthController extends Controller
 {
     protected $redirectPath = '/';
     protected $loginPath    = '/login';
-    
+
     /*
     |--------------------------------------------------------------------------
     | Registration & Login Controller
--- config/services.php
+++ config/services.php
@@ -30,7 +30,7 @@ return [
     ],
 
     'stripe' => [
-        'model'  => App\User::class,
+        'model'  => App\DataAccess\Eloquent\User::class,
         'key' => '',
         'secret' => '',
     ],
