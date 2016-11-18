c663986279a658f9d08486563417f8fff9d24705
blue-goheimochi <ooohhhaaassshhhiii@gmail.com>
2016-11-18 19:21:07 +0900

    トピック作成機能を追加

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -28,3 +28,8 @@ Route::post('password/forgot', 'Auth\PasswordController@postEmail');
 
 Route::get('password/reset/{token}', 'Auth\PasswordController@getReset');
 Route::post('password/reset', 'Auth\PasswordController@postReset')->name('password-reset');
+
+Route::get('topic/new', 'TopicController@getNewTopic')->middleware('auth')->name('new-topic');
+Route::post('topic/new', 'TopicController@postNewTopic')->middleware('auth');
+
+Route::get('topic/{id}', 'TopicController@getTopic');
